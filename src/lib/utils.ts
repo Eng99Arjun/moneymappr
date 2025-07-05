f7fd8cdcf2ce3bbc2856b11c4a390ec414aa6867
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-07-05 22:39:22 +0530

    feat: add fetchTransactions utility function to retrieve transactions from API

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,12 @@
+import axios from 'axios';
+import { Transaction } from '@/types';
+
 export function cn(...inputs: (string | false | null | undefined)[]) {
   return inputs.filter(Boolean).join(' ');
 }
+
+
+export const fetchTransactions = async (): Promise<Transaction[]> => {
+  const res = await axios.get('/api/transactions');
+  return res.data;
+};
