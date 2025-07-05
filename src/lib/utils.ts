8ab0f398df7f92ba0d00a381ac7c8265cf321a85
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-07-05 22:51:54 +0530

    refactor: remove unnecessary blank line in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,6 @@ export function cn(...inputs: (string | false | null | undefined)[]) {
   return inputs.filter(Boolean).join(' ');
 }
 
-
 export const fetchTransactions = async (): Promise<Transaction[]> => {
   const res = await axios.get('/api/transactions');
   return res.data;
